66081a3507a8fe74b1356600b11b02ea4cb66ee8
Jeevan <gairejeevan7@gmail.com>
2024-10-22 11:41:18 +0530

    Character of oop complete.

--- Object Oriented Programming/3 Principles - Inheritance, Polymorphism, Encapsulation, Abstraction/2 Polymorphism/Shapes.java
+++ Object Oriented Programming/3 Principles - Inheritance, Polymorphism, Encapsulation, Abstraction/2 Polymorphism/Shapes.java
@@ -2,4 +2,9 @@ public class Shapes {
     void area() {
         System.out.println("I am in Shapes");
     }
+
+    // Early binding as final method are not overridden so here compiler has no tension to choose which mehtod to determine
+    // final void area() {
+    //     System.out.println("I am in Shapes");
+    // }
 }
