0b7c352d879bd0d53d777b3c8947925fe13447ed
Jeevan <gairejeevan7@gmail.com>
2024-10-25 23:14:32 +0530

    Starting access control, in-built package , object class.

---
+++ Object Oriented Programming/4 Access Control, In-built Packages, Object Class/Access/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A(10, "Jeevan");
+
+        // need to do a few things
+        // 1. access the data members
+        // 2. modify the data members
+    }
+}
