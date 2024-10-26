56230d8db6d6aa3ccb5c69f16e4b6605bd6daf0e
Jeevan <gairejeevan7@gmail.com>
2024-10-26 23:14:55 +0530

    Getter and setter for private field

--- Object Oriented Programming/4 Access Control, In-built Packages, Object Class/Access/A.java
+++ Object Oriented Programming/4 Access Control, In-built Packages, Object Class/Access/A.java
@@ -1,7 +1,31 @@
 public class A {
-    int num;
+    private int num;
     String name;
     int[] arr;
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int[] getArr() {
+        return this.arr;
+    }
+
+    public void setArr(int[] arr) {
+        this.arr = arr;
+    }
 
 
     public A(int num, String name) {
