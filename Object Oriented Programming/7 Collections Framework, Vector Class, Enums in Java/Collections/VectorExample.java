8c103b7504252b87e988b63fcaf38c793a76b41c
Jeevan <gairejeevan7@gmail.com>
2024-11-04 14:22:35 +0530

    Vector example

---
+++ Object Oriented Programming/7 Collections Framework, Vector Class, Enums in Java/Collections/VectorExample.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.Vector;
+
+public class VectorExample {
+    public static void main(String[] args) {
+        
+        List<Integer> vector = new Vector<>();
+        vector.add(45);
+        vector.add(5);
+        vector.add(15);
+
+        System.out.println(vector);
+    }
+}
